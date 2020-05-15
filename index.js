5c3bf378fb23a7d71ee2e5eb2f2697d2f126fdc4
David Moreno <davidmoreno@iMac-de-David.local>
2020-05-15 13:29:55 +0200

    Creado el index js, y las dependencias necesarias

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 const app = express();
-const PORT = 3001;
+const PORT = 3001; //Añadimos puerto 3001 para evitar conflicto con React (Usa puerto 3000)
+
+// mongoose.connect('')
