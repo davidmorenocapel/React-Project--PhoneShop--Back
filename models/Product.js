467e34db40df7dfd5d9575c4153f5c2a6dc7385c
David Moreno <davidmoreno@iMac-de-David.local>
2020-05-15 17:43:43 +0200

    Modelos y controladeres creados

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,43 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.SchemaType.ObjectId;
 const ProductSchema = new mongoose.Schema({
 
-    // añadir modelos 
+    id: {
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String, //Telefono o funda
+        required: true
+    },
+    brand: {            //Marca del producto
+        type: String,
+        required: true
+    },
+    model: {            // MODELO DEL TELEFONO PARA FILTRAR 
+        type: String,
+        required: true
+    },
+    screen: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    processor: {
+        type: String,
+        required: true
+    },
+    ram: {
+        type: Number,
+        required: true
+    },
+    phone_img: {
+        type: String,
+        required: true
+    }
+});
 
-})
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
